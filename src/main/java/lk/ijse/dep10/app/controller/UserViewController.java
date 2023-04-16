efbb0ab7c903662e2d598c7aac1a5adf070256fd
Nuwan-Wimaladharma <nuwanwd20@gmail.com>
2023-04-17 00:33:56 +0530

    Completed UI design of user view

--- src/main/java/lk/ijse/dep10/app/controller/UserViewController.java
+++ src/main/java/lk/ijse/dep10/app/controller/UserViewController.java
@@ -5,6 +5,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -12,6 +14,26 @@ import java.io.IOException;
 
 public class UserViewController {
 
+    public Label lblRemainingLeaves;
+    public Label lblPendingLeaves;
+    public Label lblTakenLeaves;
+    public Label lblUnionMember;
+    public Label lblBasicSalary;
+    public Label lblDesignation;
+    public Label lblJoinedDate;
+    public Label lblNationality;
+    public Label lblMaritalStatus;
+    public Label lblGender;
+    public Label lblContact;
+    public Label lblAddress;
+    public Label lblDOB;
+    public Label lblNIC;
+    public Label lblName;
+    public Label lblID;
+    public Button btnBack;
+    public ImageView imgProfilePicture;
+    public AnchorPane panePanel;
+    public AnchorPane paneRoot;
     @FXML
     private Button btnLeave;
 
@@ -35,4 +57,6 @@ public class UserViewController {
         stage.centerOnScreen();
     }
 
+    public void btnBackOnAction(ActionEvent actionEvent) {
+    }
 }

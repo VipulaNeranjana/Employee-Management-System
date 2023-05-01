3a87719584346434115b933482be8c46d627b4d8
sagarapradeep <pradeepsyd54@gmail.com>
2023-05-01 16:00:39 +0530

    finalizing report generation-final commit

--- src/main/java/lk/ijse/dep10/app/controller/AttendanceReportViewController.java
+++ src/main/java/lk/ijse/dep10/app/controller/AttendanceReportViewController.java
@@ -1,5 +1,6 @@
 package lk.ijse.dep10.app.controller;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
@@ -8,13 +9,21 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import lk.ijse.dep10.app.db.DBConnection;
 import lk.ijse.dep10.app.util.EmployeeAttendanceReport;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.view.JasperViewer;
 
 import java.sql.*;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.HashMap;
 
 public class AttendanceReportViewController {
 
     public Button btnBack;
+    public Button btnGenerateReport;
     @FXML
     private Button btnSubmit;
 
@@ -52,13 +61,13 @@ public class AttendanceReportViewController {
 
     @FXML
     void btnSubmitOnAction(ActionEvent event) {
+
         if(!isEnteredDataValid())return;
 
         LocalDate from = dtFrom.getValue();
         LocalDate to = dtTo.getValue();
         int id = Integer.parseInt(txtID.getText());
 
-        System.out.println("Data locad");
 
         loadDataToTable(from, to, id);
 
@@ -76,40 +85,61 @@ public class AttendanceReportViewController {
                 return;
             }
 
-
             String name = resultSet.getString("name");
             lblName.setText(name);
 
-            /*Load Data to Table*/
 
-            for (LocalDate date=from; date.isBefore(to); date = date.plusDays(1)) {
-                PreparedStatement stm2 = connection.prepareStatement("SELECT *FROM Attendance WHERE id=? AND date=?");
-                stm2.setInt(1, id);
-                stm2.setDate(2, Date.valueOf(date));
-                ResultSet resultSet1 = stm2.executeQuery();
-                if(!resultSet1.next())continue;
 
-                int empId = resultSet1.getInt(1);
-                Date attendanceDate = resultSet1.getDate(2);
-                Time signInTime = resultSet1.getTime(3);
-                Time signOutTime = resultSet1.getTime(4);
 
-                Long durationMillis = Math.abs(signOutTime.getTime() - signInTime.getTime());
-                long hours = durationMillis / (1000 * 60 * 60);
-                long mints = (durationMillis % (1000 * 60 * 60)) / (1000 * 60);
 
-                String duration = "H-"+hours + " : m" + mints;
 
-                EmployeeAttendanceReport employeeAttendanceReport = new EmployeeAttendanceReport(empId, attendanceDate, signInTime, signOutTime, duration);
-                tblAttendance.getItems().add(employeeAttendanceReport);
 
-            }
 
 
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+
+        Connection connection = DBConnection.getInstance().getConnection();
+
+        PreparedStatement stm = null;
+        try {
+            stm = connection.prepareStatement("SELECT *FROM Attendance WHERE id=?");
+            stm.setInt(1,id);
+            ResultSet resultSet = stm.executeQuery();
+            while (resultSet.next()){
+
+                Long durationMillis = Math.abs(resultSet.getTime("off_time").getTime() - resultSet.getTime("in_time").getTime());
+                long hours = durationMillis / (1000 * 60 * 60);
+                long mints = (durationMillis % (1000 * 60 * 60)) / (1000 * 60);
+
+                String duration = "H-"+hours + " : m" + mints;
+
+                LocalDate dataBaseDate = resultSet.getDate("date").toLocalDate();
+                if (dataBaseDate.isEqual(to)) {
+                    EmployeeAttendanceReport empReport = new EmployeeAttendanceReport(id, resultSet.getDate("date"), resultSet.getTime("in_time"), resultSet.getTime("off_time"), duration);
+
+                    tblAttendance.getItems().add(empReport);
+
+                }if(dataBaseDate.isAfter(from)&&dataBaseDate.isBefore(to)){
+                    EmployeeAttendanceReport empReport = new EmployeeAttendanceReport(id, resultSet.getDate("date"), resultSet.getTime("in_time"), resultSet.getTime("off_time"), duration);
+
+                    tblAttendance.getItems().add(empReport);
+
+                }if(dataBaseDate.isEqual(to)){
+                    EmployeeAttendanceReport empReport = new EmployeeAttendanceReport(id, resultSet.getDate("date"), resultSet.getTime("in_time"), resultSet.getTime("off_time"), duration);
+
+                    tblAttendance.getItems().add(empReport);
+                }
+
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
 
 
     }
@@ -135,6 +165,51 @@ public class AttendanceReportViewController {
 
         stage.setScene(adminAttendanceScene);
         stage.show();
+
+    }
+
+    public void btnGenerateReportOnAction(ActionEvent actionEvent) throws JRException {
+        Connection connection = DBConnection.getInstance().getConnection();
+        try {
+            PreparedStatement preparedStatement1 = connection.prepareStatement("DROP TABLE IF EXISTS Temp_Attendance_Report");
+            preparedStatement1.execute();
+            PreparedStatement stm = connection.prepareStatement("CREATE TABLE Temp_Attendance_Report(" +
+                    "id INT NOT NULL ," +
+                    "date DATE PRIMARY KEY ," +
+                    "in_time TIME NOT NULL ," +
+                    "out_time TIME NOT NULL," +
+                    "duration VARCHAR(300) NOT NULL )");
+            stm.execute();
+
+            ObservableList<EmployeeAttendanceReport> reports = tblAttendance.getItems();
+
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO Temp_Attendance_Report (id,date,in_time,out_time,duration) VALUES (?,?,?,?,?)");
+
+            for (EmployeeAttendanceReport employee : reports) {
+                preparedStatement.setInt(1,employee.getId());
+                preparedStatement.setDate(2,Date.valueOf(employee.getDate().toString()));
+                preparedStatement.setTime(3,employee.getSignInTime());
+                preparedStatement.setTime(4,employee.getSignOutTime());
+                preparedStatement.setString(5,employee.getWorkingTime());
+                preparedStatement.executeUpdate();
+            }
+
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        JasperDesign design = JRXmlLoader.load(getClass().getResourceAsStream("/report/attendace_report.jrxml"));
+        JasperReport jasperReport = JasperCompileManager.compileReport(design);
+        Connection dataSource = DBConnection.getInstance().getConnection();
+        HashMap<String, Object> reportParams = new HashMap<>();
+
+        reportParams.put("employee_name", lblName.getText());
+
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, reportParams, dataSource);
+        JasperViewer.viewReport(jasperPrint,false);
+
 
     }
 }
